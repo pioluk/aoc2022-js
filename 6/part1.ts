6aeaacc94d093e04c0d20e353c63998cace9a843
Piotr Łukomiak <piotr.lukomiak@gmail.com>
2022-12-06 22:48:24 +0100

    Add day 6

---
+++ 6/part1.ts
@@ -0,0 +1,10 @@
+const input = Deno.readTextFileSync('./input.txt');
+
+const N = 4;
+
+for (let i = 0; i < input.length - N; ++i) {
+	if (new Set(input.slice(i, i + N).split('')).size === N) {
+		console.log(i + N);
+		break;
+	}
+}
