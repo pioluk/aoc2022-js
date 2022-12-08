64c423faa0559b7a70f85107ebb4e4e83c4bce68
Piotr Łukomiak <piotr.lukomiak@gmail.com>
2022-12-08 19:36:37 +0100

    Add day 8

---
+++ 08/part1.ts
@@ -0,0 +1,46 @@
+const input = Deno.readTextFileSync("./input.txt");
+const lines = input.split("\n");
+
+const width = lines[0].length;
+const height = lines.length;
+
+function getHeight(x: number, y: number): number {
+  return parseInt(lines[y][x]);
+}
+
+function range(start: number, end: number): number[] {
+  if (end <= start) {
+    return [];
+  }
+
+  return Array.from({ length: end - start }, (_, i) => i + start);
+}
+
+let visibleCount = width * 2 + height * 2 - 4;
+
+for (let x = 1; x < width - 1; ++x) {
+  for (let y = 1; y < height - 1; ++y) {
+    const currentHeight = getHeight(x, y);
+
+    const leftHeights = range(0, x).map((mx) => getHeight(mx, y));
+    const rightHeights = range(x + 1, width).map((mx) => getHeight(mx, y));
+    const topHeights = range(0, y).map((my) => getHeight(x, my));
+    const bottomHeights = range(y + 1, width).map((my) => getHeight(x, my));
+
+    const checkVisibility = (height: number) => height < currentHeight;
+
+    const isVisibleFromTop = topHeights.every(checkVisibility);
+    const isVisibleFromRight = rightHeights.every(checkVisibility);
+    const isVisibleFromBottom = bottomHeights.every(checkVisibility);
+    const isVisibleFromLeft = leftHeights.every(checkVisibility);
+
+    const isVisible = isVisibleFromTop || isVisibleFromRight ||
+      isVisibleFromBottom || isVisibleFromLeft;
+
+    if (isVisible) {
+      ++visibleCount;
+    }
+  }
+}
+
+console.log(visibleCount);
---
+++ 08/part2.ts
@@ -0,0 +1,48 @@
+const input = Deno.readTextFileSync("./input.txt");
+const lines = input.split("\n");
+
+const width = lines[0].length;
+const height = lines.length;
+
+function getHeight(x: number, y: number): number {
+  return parseInt(lines[y][x]);
+}
+
+function range(start: number, end: number): number[] {
+  if (end <= start) {
+    return [];
+  }
+
+  return Array.from({ length: end - start }, (_, i) => i + start);
+}
+
+function countVisibleTrees(ownHeight: number, heights: number[]): number {
+  const index = heights.findIndex((x) => x >= ownHeight);
+  return (index > -1 ? index + 1 : heights.length);
+}
+
+let score = 0;
+
+for (let x = 1; x < width - 1; ++x) {
+  for (let y = 1; y < height - 1; ++y) {
+    const currentHeight = getHeight(x, y);
+
+    const allLeftHeights = range(0, x).map((mx) => getHeight(mx, y))
+      .toReversed();
+    const allRightHeights = range(x + 1, width).map((mx) => getHeight(mx, y));
+    const allTopHeights = range(0, y).map((my) => getHeight(x, my))
+      .toReversed();
+    const allBottomHeights = range(y + 1, width).map((my) => getHeight(x, my));
+
+    const topScore = countVisibleTrees(currentHeight, allTopHeights);
+    const rightScore = countVisibleTrees(currentHeight, allRightHeights);
+    const bottomScore = countVisibleTrees(currentHeight, allBottomHeights);
+    const leftScore = countVisibleTrees(currentHeight, allLeftHeights);
+
+    const currentScore = topScore * rightScore * bottomScore * leftScore;
+
+    score = Math.max(score, currentScore);
+  }
+}
+
+console.log(score);
